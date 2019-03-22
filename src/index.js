0f0e186f88b558d804f39a77ef79df5506e5b50a
Trevor Robinson <tprobinson93@protonmail.com>
2019-03-21 17:24:05 -0700

    Lots of changes

--- src/index.js
+++ src/index.js
@@ -3,193 +3,86 @@
 const Promise = require('bluebird')
 global.Promise = Promise
 
-Promise.config({
-	// Enable warnings
-	warnings: true,
-	// Enable long stack traces
-	longStackTraces: true,
-	// Enable cancellation
-	cancellation: true,
-	// Enable monitoring
-	monitoring: true
-})
-
 process.on('unhandledRejection', function(reason, promise) {
 	console.error('unhandledRejection:', reason, promise)
 })
 
-const url = require('url')
 const requireFrom = require('requirefrom')
 const requireLib = requireFrom('lib')
-const requireRoot = requireFrom('/')
+const requireUtil = requireFrom('lib/utils')
 const logger = requireLib('logger')
-const base64url = require('base64url')
+const getAllClassesOfType = requireUtil('getAllClassesOfType')
 
-const parseArgs = requireLib('parseArgs')
+const ArgParser = requireLib('parseArgs')
 
-const args = parseArgs()
-
-const secretServiceName = 'lifevest-backup'
-
-// Put together initialization options for Dockerode
-const hostOptions = {
-	protocol: 'http',
-	port: 2375,
-	version: 'v1.37',
-	Promise,
+const main = async (input, source, output, destination) => {
+	const data = await input(source)
+	output(destination, data)
 }
 
-const parsedHostUrl = url.parse(args['--host'])
-if( parsedHostUrl.host === null && parsedHostUrl.pathname !== null ) {
-	// Plain hostname parses wrong.
-	hostOptions.host = parsedHostUrl.pathname
-} else {
-	Object.assign(hostOptions, parsedHostUrl)
-}
-
-// Set up Dockerode
-const Dockerode = require('simple-dockerode')
-const docker = new Dockerode(hostOptions)
-
-// Configure registry authentication
-const authconfig = requireRoot('registry-credentials.json')
-const defaultOptions = { authconfig }
-
-function listToMap(rawList) {
-	return rawList.reduce((acc, item) => {
-		acc[item.ID] = item.Spec
-		return acc
-	}, {})
-}
-
-async function main() {
-	// Get all services
-	const services = listToMap(await docker.listServices(defaultOptions))
-	logger.info('Retrieved services.')
-
-	// Get all configs
-	const configs = listToMap(await docker.listConfigs(defaultOptions))
-	logger.info('Retrieved configs.')
-
-	// Get all secrets
-	const secrets = listToMap(await docker.listSecrets(defaultOptions))
-	logger.info('Retrieved secret listing.')
-
-	if( Object.keys(secrets).length > 0 ) {
-		// Second step to getting all secrets: launch a special service that can retrieve secret content.
-		// Check to see that we have an active node.
-		const nodes = await docker.listNodes(defaultOptions)
-		logger.debug('Retrieved nodes:', nodes)
-		if( !nodes.some(node => node.Status.State === 'ready') ) {
-			logger.error('Secrets exist that must be retrieved, but there are no nodes that can run a service!')
-			logger.error('Secrets can only be retrieved by running a service.')
-			throw new Error()
+const initialize = async () => {
+	// Initialize all classes to retrieve dynamic flags and help text
+	let inputs, outputs
+	try {
+		inputs = await getAllClassesOfType('input')
+		outputs = await getAllClassesOfType('output')
+	} catch (e) {
+		// give a little more info from sidechannel
+		if( 'sidechannel' in e ) {
+			logger.fatal(...e.sidechannel, e)
 		}
-
-		// Create a service that does nothing, but has all the secrets.
-		const secretService = await docker.createService(Object.assign({}, defaultOptions, {
-			Name: secretServiceName,
-			Mode: { Replicated: { Replicas: 1 } },
-			TaskTemplate: {
-				ContainerSpec: {
-					Image: 'alpine:latest',
-					Command: ['sleep', '999999'],
-					Secrets: Object.keys(secrets).map(secretID => ({
-						SecretID: secretID,
-						SecretName: secrets[secretID].Name,
-						File: {
-							Name: `/${secretID}.secret`,
-							UID: '0',
-							GID: '0',
-							Mode: 666,
-						},
-					})),
-				},
-			},
-		}))
-		logger.info('Created lifevest service to retrieve secrets')
-		logger.trace(secretService)
-
-		// Create this ahead of time to be used on failures.
-		const cleanupSecretService = () => {
-			return secretService.remove()
-		}
-
-		// Wait for the service to have a working container.
-		let secretTask = null
-		let tries = 30
-		while( secretTask === null && tries > 0 ) {
-			const taskListing = await docker.listTasks({filters: JSON.stringify({service: [secretServiceName], 'desired-state': ['running']})})
-			logger.trace('Got tasks listing:', taskListing)
-
-			// As desired-state only returns the state it wants to be, not the state it is, do one more filter.
-			const possibleTask = taskListing.find(task => task.Status.State === 'running')
-
-			if( possibleTask ) {
-				secretTask = possibleTask
-			} else {
-				tries--
-				logger.trace('Waiting 1 sec. Remaining tries:', tries)
-				await Promise.delay(1000)
-			}
-		}
-
-		logger.debug('Got task:', secretTask)
-		if( !secretTask ) {
-			logger.error('Something went wrong while getting the secret-retrieval task! Cleaning up secret service...')
-			await cleanupSecretService()
-			throw new Error()
-		}
-
-		if( secretTask.ServiceID !== secretService.id || secretTask.Status.State !== 'running' ) {
-			logger.error('Something went wrong while filtering tasks! Cleaning up secret service...')
-			await cleanupSecretService()
-			throw new Error()
-		}
-
-		// Set up a new Dockerode to exec into that instance.
-		const secretServiceNode = nodes.find(node => node.ID === secretTask.NodeID)
-
-		if( !secretServiceNode ) {
-			logger.error('Could not find the node that holds the secret task! Cleaning up secret service...')
-			await cleanupSecretService()
-			throw new Error()
-		}
-
-		const secretDockerode = new Dockerode({
-			Promise,
-			host: secretServiceNode.Status.Addr,
-			protocol: 'http',
-			port: 2375,
-			version: 'v1.37',
-		})
-
-		const secretContainer = await secretDockerode.getContainer(secretTask)
-		logger.debug('Got secret container:', secretContainer)
-
-		// Grab all the secrets
-		await Promise.each(Object.keys(secrets), async secretID => {
-			// const results = await secretContainer.exec(['sh', '-c', `cat /${secretID}.secret | base64`], {stdout: true, stderr: true})
-			const results = await secretContainer.exec(['cat', `/${secretID}.secret`], {stdout: true, stderr: true})
-			if( results.inspect.ExitCode !== 0 ) {
-				// This file obviously doesn't exist, we get an exit code of 1
-				logger.error(`When getting ${secretID}, got exit ${results.inspect.ExitCode}! Stderr:`, results.stderr)
-				throw new Error(results.stderr)
-			} else {
-				logger.trace('Got secret:', secretID, results)
-				secrets[secretID].Data = base64url.encode(results.stdout)
-			}
-		})
-
-		logger.debug('Final secrets:', secrets)
-
-		logger.info('Cleaning up service.')
-		await cleanupSecretService()
+		throw e
 	}
 
-	// Now, output all of it.
-	logger.warn('Services:', services)
-	logger.warn('Configs:', configs)
-	logger.warn('Secrets:', secrets)
+	const parser = new ArgParser([...Object.values(inputs), ...Object.values(outputs)])
+	const args = parser.parse()
+
+	if( args['--help'] ) {
+		return parser.help()
+	}
+
+	// If any class needs to validate args, do so.
+	// TODO: add sidechannel errors here
+	try {
+		await Promise.each(Object.keys(inputs), key => {
+			if( 'validateArgs' in inputs[key] ) {
+				return inputs[key].validateArgs(args)
+			}
+			return true
+		})
+
+		await Promise.each(Object.keys(outputs), key => {
+			if( 'validateArgs' in outputs[key] ) {
+				return outputs[key].validateArgs(args)
+			}
+			return true
+		})
+	} catch (e) {
+		await parser.help()
+		throw e
+	}
+
+	// Configure the input class
+	const preferredInput = args['--input'] || 'swarm'
+	const source = args['--source']
+	logger.trace(inputs)
+	const input = inputs[preferredInput].method
+
+	// Configure the output class
+	const preferredOutput = args['--output'] || 'folder'
+	const destination = args['--destination'] || ('backup-' + (new Date()).toISOString())
+	const output = outputs[preferredOutput].method
+
+	// Final validation
+	if( !source ) {
+		throw new Error('No source specified, must pass --source flag!')
+	}
+
+	// Start actual work
+	return main(input, source, output, destination)
 }
-main()
+
+initialize().catch(err => {
+	logger.error(err)
+	process.exit(1)
+})
